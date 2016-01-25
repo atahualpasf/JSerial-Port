6af59c20fb57a34c8256048761cd25ce1073f7cb
Atahualpa Silva F <atahualpasilva@gmail.com>
2016-01-25 14:25:42 -0430

    Proyecto base de laboratorio de redes.

---
+++ src/Controller/MainWindowController.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Model.Communicator;
+import gnu.io.CommPortIdentifier;
+import java.util.Enumeration;
+import java.util.HashMap;
+import javax.swing.JComboBox;
+
+/**
+ *
+ * @author Atahualpa Silva F. <https://github.com/atahualpasf>
+ */
+public class MainWindowController {
+    private static JComboBox jCBPorts;
+    private static Enumeration ports = null;
+    
+    public static void initOutlets(JComboBox jCBPorts) {
+        MainWindowController.jCBPorts = jCBPorts;
+        Communicator.searchForPorts(MainWindowController.jCBPorts);
+    }
+    
+    public static boolean connectPort() {
+        Communicator.connect(jCBPorts);
+        return true;
+    }
+}
---
+++ src/View/WindowProperties.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package View;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author AtahualpaSF
+ */
+public class WindowProperties extends JFrame {
+    private JFrame  fatherWindow;
+    private boolean isFatherWindowHide;
+    private boolean isFatherWindowDisable;
+    
+
+    public JFrame getFatherWindow()
+    {
+        return fatherWindow;
+    }
+
+    public void setFatherWindow(JFrame fatherWindow, boolean hideFatherWindow)
+    {
+        this.fatherWindow = fatherWindow;
+        
+        if (hideFatherWindow)
+        {
+            this.fatherWindow.setVisible(false); 
+            this.isFatherWindowHide = true;
+            this.isFatherWindowDisable = false;
+        }
+        else
+        {
+            this.fatherWindow.setEnabled(false);
+            this.setAlwaysOnTop(true);
+            this.isFatherWindowHide = false;
+            this.isFatherWindowDisable = true;
+        }
+    }
+    
+    public void restoreFatherWindow()
+    {
+        if (this.isFatherWindowHide)
+            this.fatherWindow.setVisible(true);
+        
+        if (this.isFatherWindowDisable)
+            this.fatherWindow.setEnabled(true);
+        
+        this.dispose();
+    }
+    
+    public void setEnableWindow(boolean setEnable)
+    {
+        this.setEnabled(setEnable);
+        this.setAlwaysOnTop(true);       
+    }
+}
